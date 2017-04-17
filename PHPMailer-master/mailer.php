7d34c4c28b9d02c81e5cf4fc1e4a2c10e072fedc
Rails_course_UH <ernestas.zemaitis@gmail.com>
2017-04-17 22:50:50 +0300

    sent mail

---
+++ PHPMailer-master/mailer.php
@@ -0,0 +1,29 @@
+<?php
+require 'PHPMailerAutoload.php';
+include ".ENV.php";
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = $email;                 // SMTP username
+$mail->Password = $password;                           // SMTP password
+//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+//$mail->setFrom('ernestas.zemaitis@gmail.com', 'Mailer');
+//$mail->addAddress('ernestas@stamperija.eu', 'testas');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+//$mail->isHTML(true);                                  // Set email format to HTML
+
+
+?>
---
+++ index.php
@@ -0,0 +1,412 @@
+<?php include "includes/header.php";?>
+
+ <?php include "includes/navbar.php";?>   
+  <header class=header-oasis>
+   
+    <div class="header-oasis1 mySlides">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <div class="oasis-header">
+                        <div class="header-content-inner">
+                            <h1>Unique spot of sences</h1>
+                            <p class="p-header">Glamping stay and 360-degree view for a luxury escape and connection with surrounding.<br> Feel the moment and enjoy nature!</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-movie" align="center">
+             
+              <div class="row">
+                <div class="col-sm-3 col-sm-offset-3 highlights">
+                  <a href="https://vimeo.com/202928576" target="_blank"><p>Movie highlights <i class="icon-arrow-right-circle icon-head"></i></p></a>
+                </div>
+                <div class="col-sm-3 highlights">
+                  <a href="https://vimeo.com/184565990" target="_blank"><p>Oasis movie <i class="icon-arrow-right-circle icon-head"></i></p></a>
+                </div>
+              </div>
+           
+              <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                  <a href="#download" class="btn btn-outline btn-xl page-scroll btn-header">ORDER</a>
+                </div>
+              </div>
+            </div>
+      </div>
+    </div>
+    
+     <div class="header-oasis2 mySlides ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <div class="oasis-header">
+                            <div class="header-content-inner">
+                                <h1>Unique spot of sences</h1>
+                                <p class="p-header">Glamping stay and 360-degree view for a luxury escape and connection with surrounding.<br> Feel the moment and enjoy nature!</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-movie" align="center">
+
+                  <div class="row">
+                    <div class="col-sm-3 col-sm-offset-3 highlights">
+                      <a href="https://vimeo.com/202928576" target="_blank"><p>Movie highlights <i class="icon-arrow-right-circle icon-head"></i></p></a>
+                    </div>
+                    <div class="col-sm-3 highlights">
+                      <a href="https://vimeo.com/184565990" target="_blank"><p>Oasis movie <i class="icon-arrow-right-circle icon-head"></i></p></a>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-6 col-sm-offset-3">
+                      <a href="#download" class="btn btn-outline btn-xl page-scroll btn-header">ORDER</a>
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+        
+  </header>
+
+    <section class="download bg-primary text-center content-oasis">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <h1 class="oasis-cont">Gorgeous 360 view</h1>
+                    <p>Oasis brings totally different experience and emotions: it is a new way of staying in nature. Unique round-shaped form gives unlimited 360-degree view. Your stay in Oasis becomes a part of surroundings: no walls separation - just a clear view. It is practical, movable, long-lasting.</p>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="image-bg-oasis">
+     
+    </section>
+    
+    <section class=" text-center content-oasis">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <h1 class="oasis-cont">Ideal for your hobby</h1>
+                    <p>Oasis dome is designed to match your individual needs at various places. Spend the time for your hobby at any place you like: whether it is reading a book on a meadow or kite surfing at extremely windy places. Oasis is constructed to keep you safe inside and remain stable - no matter what’s the weather outside.</p>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="image-bg1-oasis">
+     
+    </section>
+    
+    <section class=" text-center content-oasis">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <p class="oasis-comment">"Oasis is absolutely extraordinary dome where i find myself safe and protected from enviromental effects. No matter if it's rainy or windy outside it's always cozy inside"</p>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="image-bg2-oasis">
+     
+    </section>
+    
+    <section class=" text-center content-oasis">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <h1 class="oasis-cont">Press about</h1>
+                    <p>truksta logotipu</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="image-bg2-oasis">
+     <div class="row">
+       <div class="col-md-6 col-md-offset-3">
+         <h1>Tuksta foto</h1>
+       </div>
+     </div>
+    </section>
+    
+    <section id="features" class=" text-center content-oasis content-high">
+      <div class=" ">
+        <div class="panel-heading highlights highlights-content" role="tab" id="headingOne">
+          <h1 class="oasis-cont">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              FEATURES
+            </a>
+          </h1>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+           <div class="row">
+             <div class="col-sm-6 col-sm-offset-3">
+               <img src="images/ORAI_VEJAS_SAULE.gif" class="img-responsive" alt="">
+             </div>
+           </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section class=" text-center content-oasis content-high">
+      <div class=" ">
+        <div class="panel-heading highlights highlights-content" role="tab" id="headingOne">
+          <h1 class="oasis-cont">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+              STORAGE
+            </a>
+          </h1>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+           <div class="row">
+             <div class="col-sm-6 col-sm-offset-3">
+               <img src="images/OASIS-4-DURYS-SP.gif" class="img-responsive" alt="">
+             </div>
+           </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section class=" text-center content-oasis content-high">
+      <div class=" ">
+        <div class="panel-heading highlights highlights-content" role="tab" id="headingOne">
+          <h1 class="oasis-cont">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="true" aria-controls="collapse3">
+              VENTILATION
+            </a>
+          </h1>
+        </div>
+        <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+           <div class="row">
+             <div class="col-sm-6 col-sm-offset-3">
+               <img src="images/VEDINIMAS.gif" class="img-responsive" alt="">
+             </div>
+           </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section id="download" class=" text-center content-oasis select color">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <h1 class="oasis-cont">Choose Oasis color</h1>
+                    <p>Please select Oasis color</p>
+                </div>
+            </div>
+            
+            
+            
+          <form action="shopping_card.php" method="post">    
+            <div class="row">
+              <div class="col-sm-4">
+                <label class="radio-inline">
+                  <input type="radio" name="Color[1]" value="1"  checked >Natural<img src="images/COLOR%20CANOPY%20SAND.png" class="oasis-color">
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="Color[1]" value="2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                </label>
+              </div>
+              
+                <div class="col-sm-4">
+                  <p>Construction and floor colors</p>
+                </div>
+      
+              
+                <div class="col-ms-4">
+                  <label class="radio-inline">
+                  <input type="radio" name="Color[2]" value="1" checked>Natural<img src="images/COLOR%20CANOPY%20SAND.png" class="oasis-color">
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="Color[2]" value"2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                </label>
+                </div>
+              
+            </div>
+            
+            <div class="row">
+              <div class="col-sm-4">
+                <label class="radio-inline">
+                  <input type="radio" name="Color[3]" value="1"  checked >Natural<img src="images/COLOR%20CANOPY%20SAND.png" class="oasis-color">
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="Color[3]" value="2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                </label>
+              </div>
+              
+                <div class="col-sm-4">
+                  <p>Canopy bottom construction</p>
+                </div>
+      
+              
+                <div class="col-ms-4">
+                  <label class="radio-inline">
+                  <input type="radio" name="Color[4]" value="1" checked>Natural<img src="images/COLOR%20CANOPY%20SAND.png" class="oasis-color">
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="Color[4]" value"2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                </label>
+                </div>
+              
+            </div>
+            
+             <div class="row">
+              <div class="col-sm-4">
+                <label class="radio-inline">
+                  <input type="radio" name="Color[5]" value="1"  checked >Light  beige<img src="images/COLOR%20CURTAINS%20BEIGE.png" class="oasis-color">
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="Color[5]" value="2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                </label>
+              </div>
+              
+                <div class="col-sm-4">
+                  <p>Curtains</p>
+                </div>
+      
+              
+                <div class="col-ms-4">
+                  <label class="radio-inline">
+                    <input type="radio" name="Color[6]" value="1" checked>Light beige<img src="images/COLOR%20CURTAINS%20BEIGE.png" class="oasis-color">
+                  </label>
+                  <label class="radio-inline">
+                    <input type="radio" name="Color[6]" value"2">Brown<img src="images/COLOR%20CONSTR%20BROWN.png" class="oasis-color">
+                  </label>
+                </div>
+              
+            </div>
+            
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                  <h1 class="oasis-cont">ADDS</h1>
+                </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-sm-2">
+               <div class="checkbox">
+                <label>
+                  <input type="checkbox" name="floor-heating" value="250">Floor heating
+                </label>
+                <label>
+                  <input type="checkbox" name="air-conditioning" value="190">Air conditioning
+                </label>
+                <label>
+                  <input type="checkbox" name="solar-batery" value="850">Air conditioning
+                </label>
+                
+                <label>
+                  <input type="checkbox" name="electrical-socket" value="110">Electrical socket
+                </label>
+                </div>
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <button type="submit" name="submit" class="btn btn-outline btn-xl page-scroll btn-header">Order Oasis</button>
+              </div>
+            </div>
+            
+            
+          </form>
+
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    <footer id="contact">
+        <div class="container">
+            <p>&copy; OASIS</p>
+            <ul class="list-inline">
+                <li>
+                    <a href="#">Meniu</a>
+                </li>
+                <li>
+                    <a href="#">Meniu1</a>
+                </li>
+                <li>
+                    <a href="#">Meniu2</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/jquery/fadeSlideShow.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/new-age.min.js"></script>
+    
+    <script>
+      var myIndex = 0;
+      carousel();
+
+      function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+             x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 3000); // Change image every 2 seconds
+      }
+      </script>
+
+<!--
+  <script type="text/javascript">
+    $(function() {
+    $('.fadein header:gt(0)').hide();
+
+    setInterval(function () {
+        $('.fadein :first-child').fadeOut()
+                                 .next('header')
+                                 .fadeIn()
+                                 .end()
+                                 .appendTo('.fadein');
+    }, 4000); // 4 seconds
+    });
+  </script>
+-->
+</body>
+
+</html>
